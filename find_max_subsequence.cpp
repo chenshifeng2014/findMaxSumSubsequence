94665b522d365898cfcf266a8c6170a65364f18f
chenshifeng2014 <chenshifeng365@163.com>
2015-09-06 00:24:03 +0800

    错误更正及优化

--- find_max_subsequence.cpp
+++ find_max_subsequence.cpp
@@ -2,15 +2,15 @@
 #include<vector>
 using namespace std;
 void find_max_subseq(const vector<int> &seq){
-	int max_value=0,subseq_value=0,start_pos=0,end_pos=0,tmp_pos=0;
-	for(int i=0;i<seq.size();++i){
+	int max_value=0,subseq_value=0,start_pos=0,end_pos=0,tmp_pos=1;
+	for(int i=0;i!=seq.size();++i){
 		subseq_value +=seq[i];
-		if(subseq_value>max_value){
+		if(subseq_value>=max_value){
 			max_value=subseq_value;
 			start_pos = tmp_pos;
 			end_pos = i+1;
 		} else {
-			if(subseq_value<0){
+			if(subseq_value<=0){
 				subseq_value = 0;
 				if(i!=(seq.size()-1)){
 				    tmp_pos =i+2;
@@ -21,17 +21,13 @@ void find_max_subseq(const vector<int> &seq){
 	}
 	cout<<"max_value:"<<max_value<<endl;
 	cout<<"start_pos:"<<start_pos<<endl;
-	cout<<"end_pos:"<<end_pos;
+	cout<<"end_pos:"<<end_pos<<endl;
 }
 int main(){
 	vector<int> seq;
-	int length_seq;
-	cout<<"输入序列长度"<<endl;
-	cin>>length_seq;
-	cout<<"put in the sequence,end with 00"<<endl;
+	cout<<"put in the sequence,end q"<<endl;
 	int num;
-	for(int i=0;i<length_seq;++i){
-		cin>>num;
+	while(cin>>num){
 		seq.push_back(num);
 	}
 	find_max_subseq(seq);
